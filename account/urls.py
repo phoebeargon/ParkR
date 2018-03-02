a79adbf259885cfcf4dc348a13a58fc3660f9669
Phoebe Argon <phoebeargon@gmail.com>
2018-03-02 09:45:32 -0800

    Edited change password functionality

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.auth import views as auth_views
 from .views import SignUpView, profile
 
 
@@ -10,6 +11,11 @@ urlpatterns = [
     url(r'logout$',
         LogoutView.as_view(),
         name="user_logout"),
-    url(r'signup$', SignUpView.as_view(), name='user_signup'),
+    url(r'signup$',
+        SignUpView.as_view(),
+        name='user_signup'),
     url(r'profile$', profile, name="user_profile"),
+    url(r'change-password/',
+        PasswordChangeView.as_view(template_name="account/change_password.html"),
+        name="user_password"),
 ]
